7f02aae84b5f69fd6425abac851aa321e7e63400
Ilyas Mohamed <zswaleh@ymail.com>
2023-08-14 20:05:19 -0500

    8kyu created and 1 problem solved

---
+++ 8kyu.js
@@ -0,0 +1,18 @@
+// Do problems here
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number % 2 == 0 ){
+      return "Even";
+    }else{
+      return "Odd";
+    }
+  }
+
+//   Better answer 
+
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
+
