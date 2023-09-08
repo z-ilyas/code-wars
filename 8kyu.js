49659eb094997d131c54a2c01c3c735c203bebcf
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-08 16:55:46 -0500

    division function

--- 8kyu.js
+++ 8kyu.js
@@ -73,3 +73,24 @@ function hoopCount (n) {
 function hoopCount (n) {
   return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
 }
+
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  let wow = distanceToPump/fuelLeft;
+  if( wow <= mpg){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+// Best Answer
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump/mpg <= fuelLeft
+};
