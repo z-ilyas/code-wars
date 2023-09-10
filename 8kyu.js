1c00b65203155bd0908504fc27ef128b929a2ae9
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-09 20:12:39 -0500

    conditional

--- 8kyu.js
+++ 8kyu.js
@@ -124,3 +124,27 @@ function enough(cap, on, wait) {
 function enough(cap, on, wait) {
   return Math.max(wait + on - cap, 0);
 }
+
+// // Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+function greet (name, owner) {
+  // Add code here
+  if(name === owner){
+    return 'Hello boss';
+  }else{
+    return 'Hello guest';
+  }
+}
+
+// Best Answer
+
+function greet (name, owner) {
+  return name === owner ? 'Hello boss' : 	'Hello guest';
+}
+
