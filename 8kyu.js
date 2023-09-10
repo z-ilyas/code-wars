6ea6917b0c1160c363ba07b7f4de2794767fe49d
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-10 17:22:03 -0500

    Number to String

--- 8kyu.js
+++ 8kyu.js
@@ -194,3 +194,20 @@ function squareSum(numbers){
     return (n*n) + sum;
   }, 0)
 }
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+  return num.toString();
+}
+
+// Best Answer
+
