a7e02e7af6b182d173f30d54247e7f2b2194b648
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-11 19:47:16 -0500

    array sum

--- 8kyu.js
+++ 8kyu.js
@@ -219,3 +219,21 @@ function makeUpperCase(str) {
 
 const makeUpperCase = str => str.toUpperCase();
 
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x){
+  let sum = 0;
+  for (let i of x){
+      sum += Number(i);
+  }
+   return sum;
+}
+
+// Best Answer 
+
+function sumMix(x){
+  return x.map(a => +a).reduce((a, b) => a + b);
+}
