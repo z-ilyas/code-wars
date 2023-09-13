061607cb0466e14653e94fe9928801ee5adc06cb
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-13 00:49:01 -0500

    Sum of 2 smallest numbers in the array

--- 8kyu.js
+++ 8kyu.js
@@ -265,4 +265,21 @@ function boolToWord( bool ){
   return bool ? 'Yes':'No';
 }
 
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
 
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  numbers.sort((a, b) => a - b);
+ let sum = numbers[0]+numbers[1];
+ return sum;
+ }
+
+//  Best Answer
+
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
