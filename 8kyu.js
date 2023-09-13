8385714ac1bcf4382eaff968759019975693ca60
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-13 00:28:38 -0500

    True or Not If Statement

--- 8kyu.js
+++ 8kyu.js
@@ -248,5 +248,21 @@ function solution(str){
   return str.split('').reverse().join('');
 }
 
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  //...
+  if(bool === true){
+    return "Yes";
+  }else{
+    return "No";
+  }
+}
+
+// Best Answer
+
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
+}
 
 
