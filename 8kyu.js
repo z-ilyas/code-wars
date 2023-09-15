f7492fd79273ac60a8357ca1d5897e27de89b139
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-14 23:48:40 -0500

    join strings in an array

--- 8kyu.js
+++ 8kyu.js
@@ -294,4 +294,22 @@ function maps(x){
   return x.map( num => num * 2);
   }
 
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+function smash (words) {
+  let newSentence = words.join(" ");
+   return newSentence; 
+};
+
+// Best Answer 
+
+smash = function (words) {
+  return words.join(" ");
+};
+
+
+
   
