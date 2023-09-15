64b36be375bd4757975089a4984bd5abe7f15533
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-14 23:51:14 -0500

    count items in an array

--- 8kyu.js
+++ 8kyu.js
@@ -310,6 +310,34 @@ smash = function (words) {
   return words.join(" ");
 };
 
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
+// For example,
 
-  
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
+//   The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(sheep) {
+  let counter = 0;
+  for (let shee of sheep) {
+    if (shee === true) {
+      counter += 1; // Increment the counter by 1 for each true value
+    }
+  }
+  return counter;
+}
+
+// Best Answer 
+
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
+
