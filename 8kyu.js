f2971e33dff22ab76e1d3350e074f2f804027e39
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-16 23:37:21 -0500

    Separate numbers and strings in an array

--- 8kyu.js
+++ 8kyu.js
@@ -341,29 +341,7 @@ function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(Boolean).length;
 }
 
-// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
-// Example
-// filter_list([1,2,'a','b']) == [1,2]
-// filter_list([1,'a','b',0,15]) == [1,0,15]
-// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
-
-function filter_list(l) {
-  // Return a new array with the strings filtered out
-  let New = [];
-  for (let i of l){
-    if(typeof i === 'number'){
-      New.push(i);
-    }
-  }
-  return New;
-}
-
-// Best Answer
-
-function filter_list(l) {
-  return l.filter(function(v) {return typeof v == 'number'})
-}
 
 
 
