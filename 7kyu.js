b45a21ce324bdaed1be5cea29f9ce5787653a3b4
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-17 19:59:58 -0500

    Getting the middle letter or 2 middle in an string

--- 7kyu.js
+++ 7kyu.js
@@ -21,3 +21,40 @@ function filter_list(l) {
   function filter_list(l) {
     return l.filter(function(v) {return typeof v == 'number'})
   }
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+function getMiddle(s){
+    let g = s.length;
+    let a = Math.floor(g / 2);
+  
+    if(g % 2 === 0){
+      return s.slice(a-1, a+1);
+    }else{
+      return s.charAt(a);
+    }
+  }
+
+// Best Answer
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
