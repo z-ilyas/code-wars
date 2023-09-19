bab50fa60991779444eb8803d9c992f8c19337b4
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-18 22:10:26 -0500

    make an alphabetically unique string from 2 strings

--- 7kyu.js
+++ 7kyu.js
@@ -78,3 +78,30 @@ function noSpace(x){
 function noSpace(x){
     return x.replace(/\s/g, '');
   }
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    let newString = "";
+    for (let str of [s1, s2]){
+      for (let chr of str){
+        if(!newString.includes(chr)){
+            newString += chr;
+        }
+      }
+    }
+    let alpha = newString.split('').sort().join('');
+    return alpha;
+  }
+
+// Best Answer
+
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
