96abdd84a0d28ffccbd1f02cd17e07f2d51a381b
Ilyas Mohamed <zswaleh@ymail.com>
2023-09-22 23:13:25 -0500

    added aarguments

--- 8kyu.js
+++ 8kyu.js
@@ -402,6 +402,22 @@ function getAge(inputString){
   return parseInt(inputString);
 }
 
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
 
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals +championsLeagueGoals;
+}
 
 
