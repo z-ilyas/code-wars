e213d281b5e4ea6d5abdd6929d310372d150a7f0
Ilyas Mohamed <zswaleh@ymail.com>
2023-10-31 00:11:46 -0500

    wave effect

--- 6kyu.js
+++ 6kyu.js
@@ -63,3 +63,19 @@ function deleteNth(arr,x) {
     return cache[n] <= x;
   });
 }
+
+
+// In this simple Kata your task is to create a function that turns a string into a Mexican Wave. 
+// You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+
+function wave(str){
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+        const wavedString = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+        result.push(wavedString);
+    }
+  }
+  return result;
+}
+
