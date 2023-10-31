54e6a87d6aeba2d35ad6fbc880fb2a6e58fb02b5
Ilyas Mohamed <zswaleh@ymail.com>
2023-10-30 23:14:12 -0500

    summation

--- 8kyu.js
+++ 8kyu.js
@@ -611,3 +611,23 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+var summation = function (num) {
+  if (num <= 0) {
+       return "Please enter a positive integer greater than 0.";
+   } else {
+       let total = 0;
+       for (let i = 1; i <= num; i++) {
+           total += i;
+       }
+       return total;
+   }
+}
+
